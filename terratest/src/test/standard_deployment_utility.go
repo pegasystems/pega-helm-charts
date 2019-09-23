3589c0f398eadcc43d4bf160d3c1552490b8ae83
arugm <Madhuri.Arugula@in.pega.com>
2019-09-23 19:04:24 +0530

    added standard deployment utility

--- terratest/src/test/standard_deployment_utility.go
+++ terratest/src/test/standard_deployment_utility.go
@@ -1,13 +1,17 @@
 package test
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	k8score "k8s.io/api/core/v1"
+	k8sv1beta1 "k8s.io/api/extensions/v1beta1"
 	intstr "k8s.io/apimachinery/pkg/util/intstr"
+	autoscaling "k8s.io/kubernetes/pkg/apis/autoscaling"
 )
 
 var replicas int32 = 1
@@ -48,8 +52,7 @@ func VerifyInitContinerData(t *testing.T, containers []k8score.Container) {
 	}
 }
 
-func VerifyPegaDeployment(t *testing.T, deploymentObj *appsv1.Deployment, expectedDeployment *pegaDeployment) {
-
+func VerifyPegaDeployment(t *testing.T, deploymentObj *appsv1.Deployment, expectedDeployment pegaDeployment) {
 	require.Equal(t, deploymentObj.Spec.Replicas, replicasPtr)
 	require.Equal(t, deploymentObj.Spec.ProgressDeadlineSeconds, ProgressDeadlineSecondsPtr)
 	require.Equal(t, expectedDeployment.name, deploymentObj.Spec.Selector.MatchLabels["app"])
@@ -60,16 +63,16 @@ func VerifyPegaDeployment(t *testing.T, deploymentObj *appsv1.Deployment, expect
 	require.Equal(t, expectedDeployment.name, deploymentObj.Spec.Template.Labels["app"])
 	require.NotEmpty(t, deploymentObj.Spec.Template.Annotations["config-check"])
 
-	webDeploymentSpec := deploymentObj.Spec.Template.Spec
+	deploymentSpec := deploymentObj.Spec.Template.Spec
 
-	require.Equal(t, webDeploymentSpec.Volumes[0].Name, "pega-volume-config")
-	require.Equal(t, expectedDeployment.name, webDeploymentSpec.Volumes[0].VolumeSource.ConfigMap.LocalObjectReference.Name)
-	require.Equal(t, webDeploymentSpec.Volumes[0].VolumeSource.ConfigMap.DefaultMode, volumeDefaultModePtr)
-	require.Equal(t, webDeploymentSpec.Volumes[1].Name, "pega-volume-credentials")
-	require.Equal(t, webDeploymentSpec.Volumes[1].VolumeSource.Secret.SecretName, "pega-credentials-secret")
-	require.Equal(t, webDeploymentSpec.Volumes[1].VolumeSource.Secret.DefaultMode, volumeDefaultModePtr)
+	require.Equal(t, deploymentSpec.Volumes[0].Name, "pega-volume-config")
+	require.Equal(t, expectedDeployment.name, deploymentSpec.Volumes[0].VolumeSource.ConfigMap.LocalObjectReference.Name)
+	require.Equal(t, deploymentSpec.Volumes[0].VolumeSource.ConfigMap.DefaultMode, volumeDefaultModePtr)
+	require.Equal(t, deploymentSpec.Volumes[1].Name, "pega-volume-credentials")
+	require.Equal(t, deploymentSpec.Volumes[1].VolumeSource.Secret.SecretName, "pega-credentials-secret")
+	require.Equal(t, deploymentSpec.Volumes[1].VolumeSource.Secret.DefaultMode, volumeDefaultModePtr)
 
-	actualInitContainers := webDeploymentSpec.InitContainers
+	actualInitContainers := deploymentSpec.InitContainers
 	count := len(actualInitContainers)
 	actualInitContainerNames := make([]string, count)
 	for i := 0; i < count; i++ {
@@ -78,73 +81,146 @@ func VerifyPegaDeployment(t *testing.T, deploymentObj *appsv1.Deployment, expect
 
 	require.Equal(t, expectedDeployment.initContainers, actualInitContainerNames)
 	VerifyInitContinerData(t, actualInitContainers)
-	require.Equal(t, webDeploymentSpec.Containers[0].Name, "pega-web-tomcat")
-	require.Equal(t, webDeploymentSpec.Containers[0].Image, "YOUR_PEGA_DEPLOY_IMAGE:TAG")
-	require.Equal(t, webDeploymentSpec.Containers[0].Ports[0].Name, "pega-web-port")
-	require.Equal(t, webDeploymentSpec.Containers[0].Ports[0].ContainerPort, int32(8080))
-	require.Equal(t, webDeploymentSpec.Containers[0].Env[0].Name, "NODE_TYPE")
-	require.Equal(t, expectedDeployment.nodeType, webDeploymentSpec.Containers[0].Env[0].Value)
-	require.Equal(t, webDeploymentSpec.Containers[0].Env[1].Name, "JAVA_OPTS")
-	require.Equal(t, webDeploymentSpec.Containers[0].Env[1].Value, "")
-	require.Equal(t, webDeploymentSpec.Containers[0].Env[2].Name, "INITIAL_HEAP")
-	require.Equal(t, webDeploymentSpec.Containers[0].Env[2].Value, "4096m")
-	require.Equal(t, webDeploymentSpec.Containers[0].Env[3].Name, "MAX_HEAP")
-	require.Equal(t, webDeploymentSpec.Containers[0].Env[3].Value, "7168m")
-	require.Equal(t, webDeploymentSpec.Containers[0].EnvFrom[0].ConfigMapRef.LocalObjectReference.Name, "pega-environment-config")
+	require.Equal(t, deploymentSpec.Containers[0].Name, "pega-web-tomcat")
+	require.Equal(t, deploymentSpec.Containers[0].Image, "YOUR_PEGA_DEPLOY_IMAGE:TAG")
+	require.Equal(t, deploymentSpec.Containers[0].Ports[0].Name, "pega-web-port")
+	require.Equal(t, deploymentSpec.Containers[0].Ports[0].ContainerPort, int32(8080))
+	require.Equal(t, deploymentSpec.Containers[0].Env[0].Name, "NODE_TYPE")
+	require.Equal(t, expectedDeployment.nodeType, deploymentSpec.Containers[0].Env[0].Value)
+	require.Equal(t, deploymentSpec.Containers[0].Env[1].Name, "JAVA_OPTS")
+	require.Equal(t, deploymentSpec.Containers[0].Env[1].Value, "")
+	require.Equal(t, deploymentSpec.Containers[0].Env[2].Name, "INITIAL_HEAP")
+	require.Equal(t, deploymentSpec.Containers[0].Env[2].Value, "4096m")
+	require.Equal(t, deploymentSpec.Containers[0].Env[3].Name, "MAX_HEAP")
+	require.Equal(t, deploymentSpec.Containers[0].Env[3].Value, "7168m")
+	require.Equal(t, deploymentSpec.Containers[0].EnvFrom[0].ConfigMapRef.LocalObjectReference.Name, "pega-environment-config")
 
 	//degah
-	//require.Equal(t, webDeploymentSpec.Containers[0].Resources.Limits[k8score.ResourceName("cpu")].Fromat, k8sresource.Quantity)
-	//require.Equal(t, webDeploymentSpec.Containers[0].Resources.Limits[k8score.ResourceName("memory")], "8Gi")
-	//require.Equal(t, webDeploymentSpec.Containers[0].Resources.Requests[k8score.ResourceName("cpu")].Fromat, k8sresource.Quantity)
-	//require.Equal(t, webDeploymentSpec.Containers[0].Resources.Requests[k8score.ResourceName("memory")], "6Gi")
+	//require.Equal(t, deploymentSpec.Containers[0].Resources.Limits[k8score.ResourceName("cpu")].Fromat, k8sresource.Quantity)
+	//require.Equal(t, deploymentSpec.Containers[0].Resources.Limits[k8score.ResourceName("memory")], "8Gi")
+	//require.Equal(t, deploymentSpec.Containers[0].Resources.Requests[k8score.ResourceName("cpu")].Fromat, k8sresource.Quantity)
+	//require.Equal(t, deploymentSpec.Containers[0].Resources.Requests[k8score.ResourceName("memory")], "6Gi")
 
-	require.Equal(t, webDeploymentSpec.Containers[0].VolumeMounts[0].Name, "pega-volume-config")
-	require.Equal(t, webDeploymentSpec.Containers[0].VolumeMounts[0].MountPath, "/opt/pega/config")
-	require.Equal(t, webDeploymentSpec.Containers[0].VolumeMounts[1].Name, "pega-volume-credentials")
-	require.Equal(t, webDeploymentSpec.Containers[0].VolumeMounts[1].MountPath, "/opt/pega/secrets")
+	require.Equal(t, deploymentSpec.Containers[0].VolumeMounts[0].Name, "pega-volume-config")
+	require.Equal(t, deploymentSpec.Containers[0].VolumeMounts[0].MountPath, "/opt/pega/config")
+	require.Equal(t, deploymentSpec.Containers[0].VolumeMounts[1].Name, "pega-volume-credentials")
+	require.Equal(t, deploymentSpec.Containers[0].VolumeMounts[1].MountPath, "/opt/pega/secrets")
 
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.InitialDelaySeconds, int32(300))
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.TimeoutSeconds, int32(20))
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.PeriodSeconds, int32(10))
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.SuccessThreshold, int32(1))
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.FailureThreshold, int32(3))
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.HTTPGet.Path, "/prweb/PRRestService/monitor/pingService/ping")
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.HTTPGet.Port, intstr.FromInt(8080))
-	require.Equal(t, webDeploymentSpec.Containers[0].LivenessProbe.HTTPGet.Scheme, k8score.URIScheme("HTTP"))
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.InitialDelaySeconds, int32(300))
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.TimeoutSeconds, int32(20))
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.PeriodSeconds, int32(10))
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.SuccessThreshold, int32(1))
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.FailureThreshold, int32(3))
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.HTTPGet.Path, "/prweb/PRRestService/monitor/pingService/ping")
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.HTTPGet.Port, intstr.FromInt(8080))
+	require.Equal(t, deploymentSpec.Containers[0].LivenessProbe.HTTPGet.Scheme, k8score.URIScheme("HTTP"))
 
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.InitialDelaySeconds, int32(300))
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.TimeoutSeconds, int32(20))
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.PeriodSeconds, int32(10))
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.SuccessThreshold, int32(1))
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.FailureThreshold, int32(3))
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.HTTPGet.Path, "/prweb/PRRestService/monitor/pingService/ping")
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.HTTPGet.Port, intstr.FromInt(8080))
-	require.Equal(t, webDeploymentSpec.Containers[0].ReadinessProbe.HTTPGet.Scheme, k8score.URIScheme("HTTP"))
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.InitialDelaySeconds, int32(300))
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.TimeoutSeconds, int32(20))
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.PeriodSeconds, int32(10))
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.SuccessThreshold, int32(1))
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.FailureThreshold, int32(3))
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.HTTPGet.Path, "/prweb/PRRestService/monitor/pingService/ping")
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.HTTPGet.Port, intstr.FromInt(8080))
+	require.Equal(t, deploymentSpec.Containers[0].ReadinessProbe.HTTPGet.Scheme, k8score.URIScheme("HTTP"))
 
-	require.Equal(t, webDeploymentSpec.ImagePullSecrets[0].Name, "pega-registry-secret")
-	require.Equal(t, webDeploymentSpec.RestartPolicy, k8score.RestartPolicy("Always"))
-	require.Equal(t, webDeploymentSpec.TerminationGracePeriodSeconds, terminationGracePeriodSecondsPtr)
-	// HPA
-
-	// resource limits
-
-	// rolling updates
-
-	//
+	require.Equal(t, deploymentSpec.ImagePullSecrets[0].Name, "pega-registry-secret")
+	require.Equal(t, deploymentSpec.RestartPolicy, k8score.RestartPolicy("Always"))
+	require.Equal(t, deploymentSpec.TerminationGracePeriodSeconds, terminationGracePeriodSecondsPtr)
 }
 
-func VerifyPegaServices()
+type pegaServices struct {
+	Name       string
+	Port       int32
+	TargetPort int32
+}
 
-func VerifyPegaIngress()
+func VerifyPegaServices(t *testing.T, serviceObj *k8score.Service, expectedService pegaServices) {
+	require.Equal(t, serviceObj.Spec.Selector["app"], expectedService.Name)
+	require.Equal(t, serviceObj.Spec.Ports[0].Port, expectedService.Port)
+	require.Equal(t, serviceObj.Spec.Ports[0].TargetPort, expectedService.TargetPort)
+	require.Equal(t, serviceObj.Annotations["traefik.ingress.kubernetes.io/affinity"], "true")
+	require.Equal(t, serviceObj.Annotations["traefik.ingress.kubernetes.io/load-balancer-method"], "drr")
+	require.Equal(t, serviceObj.Annotations["traefik.ingress.kubernetes.io/max-conn-amount"], "10")
+	require.Equal(t, serviceObj.Annotations["traefik.ingress.kubernetes.io/session-cookie-name"], "UNIQUE-PEGA-COOKIE-NAME")
+}
 
-func VerifyPegaStatefulset()
+type pegaIngress struct {
+	Name string
+	Port int32
+}
+
+func VerifyPegaIngress(t *testing.T, ingressObj *k8sv1beta1.Ingress, expectedIngress pegaIngress) {
+	require.Equal(t, ingressObj.Annotations["kubernetes.io/ingress.class"], "traefik")
+	require.Equal(t, ingressObj.Spec.Rules[0].HTTP.Paths[0].Backend.ServiceName, expectedIngress.Name)
+	require.Equal(t, ingressObj.Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort, expectedIngress.Port)
+}
+
+func VerifyPegaStatefulset() {
+
+}
 
 // Just verify what is exposed in the values yaml & k8s objects
-func VerifyCassandraService()
+func VerifyCassandraService() {
+
+}
 
 // Just verify what is exposed in the values yaml & k8s objects
-func VerifySearchService()
+func VerifySearchService(t *testing.T, searchServiceObj *k8score.Service) {
+	require.Equal(t, searchServiceObj.Spec.Selector["component"], "Search")
+	require.Equal(t, searchServiceObj.Spec.Selector["app"], "pega-search")
+	require.Equal(t, searchServiceObj.Spec.Ports[0].Name, "http")
+	require.Equal(t, searchServiceObj.Spec.Ports[0].Port, int32(80))
+	require.Equal(t, searchServiceObj.Spec.Ports[0].TargetPort, intstr.FromInt(9200))
+}
 
-func VefifyEnvironmentConfig()
+// VerifyEnvironmentConfig
+func VerifyEnvironmentConfig(t *testing.T, envConfigObj k8score.ConfigMap) {
+	envConfigData := envConfigObj.Data
+	require.Equal(t, envConfigData["DB_TYPE"], "YOUR_DATABASE_TYPE")
+	require.Equal(t, envConfigData["JDBC_URL"], "YOUR_JDBC_URL")
+	require.Equal(t, envConfigData["JDBC_CLASS"], "YOUR_JDBC_DRIVER_CLASS")
+	require.Equal(t, envConfigData["JDBC_DRIVER_URI"], "YOUR_JDBC_DRIVER_URI")
+	require.Equal(t, envConfigData["RULES_SCHEMA"], "YOUR_RULES_SCHEMA")
+	require.Equal(t, envConfigData["DATA_SCHEMA"], "YOUR_DATA_SCHEMA")
+	require.Equal(t, envConfigData["CUSTOMERDATA_SCHEMA"], "")
+	require.Equal(t, envConfigData["JDBC_CONNECTION_PROPERTIES"], "socketTimeout=90")
+	require.Equal(t, envConfigData["PEGA_SEARCH_URL"], "http://pega-search")
+	require.Equal(t, envConfigData["CASSANDRA_CLUSTER"], "true")
+	require.Equal(t, envConfigData["CASSANDRA_NODES"], "release-name-cassandra")
+	require.Equal(t, envConfigData["CASSANDRA_PORT"], "9042")
+}
 
-func VerifyTierConfig()
+//VerifyTierConfig
+func VerifyTierConfig(t *testing.T, configObj k8score.ConfigMap) {
+	pegaConfigMapData := configObj.Data
+	compareConfigMapData(t, []byte(pegaConfigMapData["prconfig.xml"]), "expectedInstallDeployPrconfig.xml")
+	compareConfigMapData(t, []byte(pegaConfigMapData["context.xml.tmpl"]), "expectedInstallDeployContext.xml")
+	compareConfigMapData(t, []byte(pegaConfigMapData["prlog4j2.xml"]), "expectedInstallDeployPRlog4j2.xml")
+}
+
+// util function for comparing
+func compareConfigMapData(t *testing.T, actualFile []byte, expectedFileName string) {
+	expectedPrconfig, err := ioutil.ReadFile(expectedFileName)
+	require.Empty(t, err)
+
+	equal := bytes.Equal(expectedPrconfig, actualFile)
+	require.Equal(t, true, equal)
+}
+
+type hpa struct {
+	name          string
+	targetRefName string
+	kind          string
+	apiversion    string
+}
+
+func VerifyPegaHpa(t *testing.T, hpaObj *autoscaling.HorizontalPodAutoscaler, expectedHpa hpa) {
+	require.Equal(t, hpaObj.Name, expectedHpa.name)
+	require.Equal(t, hpaObj.Spec.ScaleTargetRef.Name, expectedHpa.targetRefName)
+	require.Equal(t, hpaObj.Spec.ScaleTargetRef.Kind, expectedHpa.kind)
+	require.Equal(t, hpaObj.Spec.ScaleTargetRef.APIVersion, expectedHpa.apiversion)
+
+	require.Equal(t, hpaObj.Spec.MinReplicas, replicasPtr)
+	require.Equal(t, hpaObj.Spec.MaxReplicas, int32(5))
+}
