62b5618ef143a45468cde0ca792ddc78b8b18efa
levim2 <maxim.levitskiy@pega.com>
2020-01-21 13:32:12 +0000

    Fix terratest for be able to run go test instead of use different file lists

--- terratest/src/test/install_deploy_test.go
+++ terratest/src/test/install_deploy_test.go
@@ -9,10 +9,8 @@ import (
 	k8sbatch "k8s.io/api/batch/v1"
 )
 
-const pegaHelmChartPath = "../../../charts/pega"
-
 // Sets the the action to install-deploy, all test cases present in this file uses this action
-var options = &helm.Options{
+var installDeployOptions = &helm.Options{
 	SetValues: map[string]string{
 		"global.provider":        "k8s",
 		"global.actions.execute": "install-deploy",
@@ -21,7 +19,7 @@ var options = &helm.Options{
 
 // VerifyInstallDeployActionSkippedTemplates - Tests all the skipped templates for action install-deploy. These templates not supposed to be rendered for install-deploy action.
 func VerifyInstallDeployActionSkippedTemplates(t *testing.T) {
-	output := helm.RenderTemplate(t, options, pegaHelmChartPath, []string{
+	output := helm.RenderTemplate(t, installDeployOptions, PegaHelmChartPath, []string{
 		"templates/pega-action-validate.yaml",
 		"charts/installer/templates/pega-upgrade-environment-config.yaml",
 	})
@@ -35,18 +33,18 @@ func VerifyInstallDeployActionSkippedTemplates(t *testing.T) {
 // VerifyInstallDeployActionInstallerJob - Tests Install job yaml rendered with the values as provided in default values.yaml for action install-deploy
 func VerifyInstallDeployActionInstallerJob(t *testing.T) {
 	var installerJobObj k8sbatch.Job
-	var installerSlice = ReturnJobSlices(t, pegaHelmChartPath, options)
+	var installerSlice = ReturnJobSlices(t, PegaHelmChartPath, installDeployOptions)
 	helm.UnmarshalK8SYaml(t, installerSlice[1], &installerJobObj)
-	VerifyPegaJob(t, options, &installerJobObj, pegaJob{"pega-db-install", []string{}, "pega-install-environment-config"})
+	VerifyPegaJob(t, installDeployOptions, &installerJobObj, pegaJob{"pega-db-install", []string{}, "pega-install-environment-config"})
 }
 
 // TestInstallDeployActions - Test all objects deployed for install-deploy action with the values as provided in default values.yaml
 func TestInstallDeployActions(t *testing.T) {
 	VerifyInstallDeployActionSkippedTemplates(t)
 	VerifyInstallDeployActionInstallerJob(t)
-	VerifyInstallerConfigMaps(t, options, pegaHelmChartPath)
-	VerifyInstallEnvConfig(t, options, pegaHelmChartPath)
-	VerifyInstallerRoleBinding(t, options, pegaHelmChartPath)
-	VerifyInstallerRole(t, options, pegaHelmChartPath)
-	VerifyPegaStandardTierDeployment(t, pegaHelmChartPath, options, []string{"wait-for-pegainstall", "wait-for-pegasearch", "wait-for-cassandra"})
+	VerifyInstallerConfigMaps(t, installDeployOptions, PegaHelmChartPath)
+	VerifyInstallEnvConfig(t, installDeployOptions, PegaHelmChartPath)
+	VerifyInstallerRoleBinding(t, installDeployOptions, PegaHelmChartPath)
+	VerifyInstallerRole(t, installDeployOptions, PegaHelmChartPath)
+	VerifyPegaStandardTierDeployment(t, PegaHelmChartPath, installDeployOptions, []string{"wait-for-pegainstall", "wait-for-pegasearch", "wait-for-cassandra"})
 }
