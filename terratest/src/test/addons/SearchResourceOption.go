33ea216a5f8e7db1b034445da8898708d5b4d88f
bkk01 <kinga.bak@pega.com>
2020-02-04 13:43:09 +0100

    Changes to traefik_test.go and SearchResourceOption.go

--- terratest/src/test/addons/SearchResourceOption.go
+++ terratest/src/test/addons/SearchResourceOption.go
@@ -4,38 +4,3 @@ type SearchResourceOption struct {
 	name string
 	kind string
 }
-
-var traefikResources = []SearchResourceOption{
-	{
-		name: "release-name-traefik",
-		kind: "ConfigMap",
-	},
-	{
-		name: "release-name-traefik",
-		kind: "ServiceAccount",
-	},
-	{
-		name: "release-name-traefik",
-		kind: "ClusterRole",
-	},
-	{
-		name: "release-name-traefik",
-		kind: "Deployment",
-	},
-	{
-		name: "release-name-traefik",
-		kind: "ClusterRoleBinding",
-	},
-	{
-		name: "release-name-traefik",
-		kind: "Service",
-	},
-	{
-		name: "release-name-traefik-test",
-		kind: "Pod",
-	},
-	{
-		name: "release-name-traefik-test",
-		kind: "ConfigMap",
-	},
-}
