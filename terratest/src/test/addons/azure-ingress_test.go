6bb854b6359a8e93d255436825c40456f0a40982
mieka1979 <ania.miekus@gmail.com>
2020-02-06 09:49:46 +0100

    Azure Ingress refactored

--- terratest/src/test/addons/azure-ingress_test.go
+++ terratest/src/test/addons/azure-ingress_test.go
@@ -1,7 +1,9 @@
 package addons
 
 import (
+	b64 "encoding/base64"
 	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
 	"test/testhelpers"
 	"testing"
 )
@@ -34,6 +36,46 @@ func TestAzureIngressShouldContainAllResources(t *testing.T) {
 			Kind: i.Kind,
 		}))
 	}
+}
+
+func TestSetValuesForAppGW(t *testing.T) {
+	helmChart := testhelpers.NewHelmConfigParser(
+		testhelpers.NewHelmTest(t, helmChartRelativePath, map[string]string{
+			"ingress-azure.enabled":              "true",
+			"ingress-azure.appgw.subscriptionId": "<YOUR.SUBSCRIPTION_ID>",
+			"ingress-azure.appgw.resourceGroup":  "<RESOURCE_GROUP_NAME>",
+			"ingress-azure.appgw.name":           "<APPLICATION_GATEWAY_NAME>",
+		}),
+	)
+
+	var configMap *v1.ConfigMap
+	helmChart.Find(testhelpers.SearchResourceOption{
+		Name: "release-name-cm-ingress-azure",
+		Kind: "ConfigMap",
+	}, &configMap)
+
+	require.Equal(t, "<YOUR.SUBSCRIPTION_ID>", configMap.Data["APPGW_SUBSCRIPTION_ID"])
+	require.Equal(t, "<RESOURCE_GROUP_NAME>", configMap.Data["APPGW_RESOURCE_GROUP"])
+	require.Equal(t, "<APPLICATION_GATEWAY_NAME>", configMap.Data["APPGW_NAME"])
+	require.Equal(t, "<APPLICATION_GATEWAY_NAME>-subnet", configMap.Data["APPGW_SUBNET_NAME"])
+}
+
+func TestSetValuesForArmAuth(t *testing.T) {
+	helmChart := testhelpers.NewHelmConfigParser(
+		testhelpers.NewHelmTest(t, helmChartRelativePath, map[string]string{
+			"ingress-azure.enabled":            "true",
+			"ingress-azure.armAuth.type":       "servicePrincipal",
+			"ingress-azure.armAuth.secretJSON": b64.StdEncoding.EncodeToString([]byte("<SECRET_JSON_CREATED_USING_ABOVE_COMMAND>")),
+		}),
+	)
+
+	var secret *v1.Secret
+	helmChart.Find(testhelpers.SearchResourceOption{
+		Name: "networking-appgw-k8s-azure-service-principal",
+		Kind: "Secret",
+	}, &secret)
+
+	require.Equal(t, "<SECRET_JSON_CREATED_USING_ABOVE_COMMAND>", string(secret.Data["armAuth.json"]))
 }
 
 var azureIngressResources = []testhelpers.SearchResourceOption{
