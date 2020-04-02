ae4ed6588ea23c9f3041287270fb5adcd00dec41
anuds <Santosh.Anudeep@in.pega.com>
2020-04-02 16:57:50 +0530

    Modified constellation tests.

--- terratest/src/test/pega/deploy_with_constellation_test.go
+++ terratest/src/test/pega/deploy_with_constellation_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 // set action execute to install
-var options = &helm.Options{
+var constellation_options = &helm.Options{
 	SetValues: map[string]string{
 		"global.provider":        "k8s",
 		"global.actions.execute": "deploy",
@@ -19,11 +19,11 @@ var options = &helm.Options{
 
 // TestPegaStandardTierDeployment - Test case to verify the standard pega tier deployment.
 // Standard tier deployment includes web deployment, batch deployment, stream statefulset, search service, hpa, rolling update, web services, ingresses and config maps
-func TestPegaStandardTierDeployment(t *testing.T) {
+func TestPegaStandardTierDeploymentWithConstellation(t *testing.T) {
 	t.Parallel()
 	// Path to the helm chart we will test
 	helmChartPath, err := filepath.Abs(PegaHelmChartPath)
 	require.NoError(t, err)
 
-	VerifyPegaStandardTierDeployment(t, helmChartPath, options, []string{"wait-for-pegasearch", "wait-for-cassandra"})
+	VerifyPegaStandardTierDeployment(t, helmChartPath, constellation_options, []string{"wait-for-pegasearch", "wait-for-cassandra"})
 }
