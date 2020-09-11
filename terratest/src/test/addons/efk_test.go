1dea3db4aa66a37945fa45e042592cd535697a5a
MadhuriArugula <madhuriarugula77@gmail.com>
2020-09-11 16:10:12 +0530

    Updated EFK addons tests with version upgrade

--- terratest/src/test/addons/efk_test.go
+++ terratest/src/test/addons/efk_test.go
@@ -3,15 +3,13 @@ package addons
 import (
 	"github.com/stretchr/testify/require"
 	"k8s.io/api/apps/v1beta2"
-	v1 "k8s.io/api/core/v1"
 	"k8s.io/api/extensions/v1beta1"
-	"test/common"
 	"testing"
 )
 
 func TestShouldNotContainDeploy_EFKIfDisabled(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
 			"elasticsearch.enabled":         "false",
 			"kibana.enabled":                "false",
 			"fluentd-elasticsearch.enabled": "false",
@@ -19,15 +17,16 @@ func TestShouldNotContainDeploy_EFKIfDisabled(t *testing.T) {
 	)
 
 	for _, i := range deployEfkResources {
-		require.False(t, helmChartParser.Contains(common.SearchResourceOption{
+		require.False(t, helmChartParser.Contains(SearchResourceOption{
 			Name: i.Name,
 			Kind: i.Kind,
 		}))
 	}
 }
+
 func TestShouldDeploy_EFKContainAllResourcesIfEnabled(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
 			"elasticsearch.enabled":         "true",
 			"kibana.enabled":                "true",
 			"fluentd-elasticsearch.enabled": "true",
@@ -35,89 +34,44 @@ func TestShouldDeploy_EFKContainAllResourcesIfEnabled(t *testing.T) {
 	)
 
 	for _, i := range deployEfkResources {
-		require.True(t, helmChartParser.Contains(common.SearchResourceOption{
+		require.True(t, helmChartParser.Contains(SearchResourceOption{
 			Name: i.Name,
 			Kind: i.Kind,
 		}))
 	}
 }
 
-func Test_shouldBeFullnameOverrideForElasticsearch(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
-			"elasticsearch.enabled": "true",
-		}),
-	)
-	require.True(t, helmChartParser.Contains(common.SearchResourceOption{
-		Name: "elastic-search",
-		Kind: "ConfigMap",
-	}))
-}
-
-func Test_shouldBeElasticSearchUrlForKibana(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
-			"kibana.enabled": "true",
-			"kibana.files.kibana.yml.elasticsearch.url": "http://elastic-search-client:9200",
-		}),
-	)
-
-	var configmap *v1.ConfigMap
-	helmChartParser.Find(common.SearchResourceOption{
-		Name: "release-name-kibana",
-		Kind: "ConfigMap",
-	}, &configmap)
-
-	require.Contains(t, configmap.Data["kibana.yml"], "http://elastic-search-client:9200")
-}
-func Test_shouldBeServiceExternalPortForKibana(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
-			"kibana.enabled":              "true",
-			"kibana.service.externalPort": "80",
-		}),
-	)
-
-	var service *v1.Service
-	helmChartParser.Find(common.SearchResourceOption{
-		Name: "release-name-kibana",
-		Kind: "Service",
-	}, &service)
-
-	require.Equal(t, int32(80), service.Spec.Ports[0].Port)
-}
-
 func Test_shouldBeIngressEnabledForKibana(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
 			"kibana.enabled":         "true",
 			"kibana.ingress.enabled": "true",
 		}),
 	)
 
-	require.True(t, helmChartParser.Contains(common.SearchResourceOption{
-		Name: "release-name-kibana",
+	require.True(t, helmChartParser.Contains(SearchResourceOption{
+		Name: "pega-kibana",
 		Kind: "Ingress",
 	}))
 }
 
 func Test_shouldBeIngressDisabledForKibana(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
 			"kibana.enabled":         "true",
 			"kibana.ingress.enabled": "false",
 		}),
 	)
 
-	require.False(t, helmChartParser.Contains(common.SearchResourceOption{
-		Name: "release-name-kibana",
+	require.False(t, helmChartParser.Contains(SearchResourceOption{
+		Name: "pega-kibana",
 		Kind: "Ingress",
 	}))
 }
 
 func Test_shouldBeHostForIngressKibana(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
 			"kibana.enabled":         "true",
 			"kibana.ingress.enabled": "true",
 			"kibana.ingress.hosts":   "{YOUR_WEB.KIBANA.EXAMPLE.COM}",
@@ -125,8 +79,8 @@ func Test_shouldBeHostForIngressKibana(t *testing.T) {
 	)
 
 	var ingress *v1beta1.Ingress
-	helmChartParser.Find(common.SearchResourceOption{
-		Name: "release-name-kibana",
+	helmChartParser.Find(SearchResourceOption{
+		Name: "pega-kibana",
 		Kind: "Ingress",
 	}, &ingress)
 
@@ -134,123 +88,61 @@ func Test_shouldBeHostForIngressKibana(t *testing.T) {
 
 }
 func Test_shouldBeHostForElasticsearch(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
 			"fluentd-elasticsearch.enabled": "true",
-			"elasticsearch.host":            "elastic-search-client",
+			"fluentd-elasticsearch.elasticsearch.host": "elasticsearch-master:9200",
 		}),
 	)
 
 	var daemon *v1beta2.DaemonSet
-	helmChartParser.Find(common.SearchResourceOption{
-		Name: "release-name-fluentd-elasticsearch",
+	helmChartParser.Find(SearchResourceOption{
+		Name: "pega-fluentd-elasticsearch",
 		Kind: "DaemonSet",
 	}, &daemon)
 
-	require.Equal(t, "elastic-search-client", daemon.Spec.Template.Spec.Containers[0].Env[1].Value)
+	require.Equal(t, "elasticsearch-master:9200", daemon.Spec.Template.Spec.Containers[0].Env[1].Value)
 }
 
-func Test_shouldBeBufferChunkLimitForElasticsearch(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
-			"fluentd-elasticsearch.enabled":    "true",
-			"elasticsearch.buffer_chunk_limit": "250M",
-		}),
-	)
-
-	var daemon *v1beta2.DaemonSet
-	helmChartParser.Find(common.SearchResourceOption{
-		Name: "release-name-fluentd-elasticsearch",
-		Kind: "DaemonSet",
-	}, &daemon)
-
-	require.Equal(t, "250M", daemon.Spec.Template.Spec.Containers[0].Env[4].Value)
-}
-
-func Test_shouldBeBufferQueueLimitForElasticsearch(t *testing.T) {
-	helmChartParser := common.NewHelmConfigParser(
-		common.NewHelmTest(t, helmChartRelativePath, map[string]string{
-			"fluentd-elasticsearch.enabled":    "true",
-			"elasticsearch.buffer_queue_limit": "30",
-		}),
-	)
-
-	var daemon *v1beta2.DaemonSet
-	helmChartParser.Find(common.SearchResourceOption{
-		Name: "release-name-fluentd-elasticsearch",
-		Kind: "DaemonSet",
-	}, &daemon)
-
-	require.Equal(t, "30", daemon.Spec.Template.Spec.Containers[0].Env[5].Value)
-}
-
-var deployEfkResources = []common.SearchResourceOption{
+var deployEfkResources = []SearchResourceOption{
 	{
-		Name: "release-name-kibana",
-		Kind: "ConfigMap",
-	},
-	{
-		Name: "release-name-kibana",
+		Name: "pega-kibana",
 		Kind: "Service",
 	},
 	{
-		Name: "release-name-kibana",
+		Name: "pega-kibana",
 		Kind: "Deployment",
 	},
 	{
-		Name: "elastic-search",
-		Kind: "ConfigMap",
-	},
+		Name: "pega-kibana",
+		Kind: "Ingress",
+	},	
 	{
-		Name: "elastic-search-data",
+		Name: "elasticsearch-master",
 		Kind: "StatefulSet",
 	},
 	{
-		Name: "elastic-search-master",
-		Kind: "StatefulSet",
+		Name: "elasticsearch-master-pdb",
+		Kind: "PodDisruptionBudget",
 	},
 	{
-		Name: "elastic-search-client",
-		Kind: "ServiceAccount",
-	},
-	{
-		Name: "elastic-search-data",
-		Kind: "ServiceAccount",
-	},
-	{
-		Name: "elastic-search-master",
-		Kind: "ServiceAccount",
-	},
-	{
-		Name: "elastic-search-client",
+		Name: "elasticsearch-master",
 		Kind: "Service",
 	},
 	{
-		Name: "elastic-search-discovery",
+		Name: "elasticsearch-master-headless",
 		Kind: "Service",
 	},
 	{
-		Name: "elastic-search-client",
-		Kind: "Deployment",
-	},
-	{
-		Name: "release-name-fluentd-elasticsearch",
+		Name: "pega-fluentd-elasticsearch",
 		Kind: "ConfigMap",
 	},
 	{
-		Name: "release-name-fluentd-elasticsearch",
+		Name: "pega-fluentd-elasticsearch",
 		Kind: "ServiceAccount",
 	},
 	{
-		Name: "release-name-fluentd-elasticsearch",
-		Kind: "ClusterRole",
-	},
-	{
-		Name: "release-name-fluentd-elasticsearch",
-		Kind: "ClusterRoleBinding",
-	},
-	{
-		Name: "release-name-fluentd-elasticsearch",
+		Name: "pega-fluentd-elasticsearch",
 		Kind: "DaemonSet",
 	},
 }
