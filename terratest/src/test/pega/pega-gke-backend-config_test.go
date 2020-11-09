4af84e0f8ae41f1936cbf32d0e25b1341757a25c
vasak <KishoreKumar.Vasantala@in.pega.com>
2020-11-09 20:04:03 +0530

    ISSUE-135: added tier wise gke back config verification

--- terratest/src/test/pega/pega-gke-backend-config_test.go
+++ terratest/src/test/pega/pega-gke-backend-config_test.go
@@ -1,10 +1,12 @@
 package pega
 
 import (
+	"fmt"
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/stretchr/testify/require"
 	"k8s.io/ingress-gce/pkg/apis/backendconfig/v1beta1"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -25,19 +27,40 @@ func TestPegaGKEBackendConfig(t *testing.T) {
 		    }
 
 			yamlContent := RenderTemplate(t, options, helmChartPath, []string{"templates/pega-gke-backend-config.yaml"})
-			verifyBackendConfig(t, yamlContent)			
-
+			fmt.Print(yamlContent)
+			verifyBackendConfigs(t, yamlContent)
 		}
-
 }
 
-func verifyBackendConfig(t *testing.T, yamlContent string) {
+func verifyBackendConfigs(t *testing.T, yamlContent string) {
 	var backendConfig v1beta1.BackendConfig
-	UnmarshalK8SYaml(t, yamlContent, &backendConfig)
+	backendConfigSlice := strings.Split(yamlContent, "---")
+	for index, backendConfigStr := range backendConfigSlice {
+		if index >= 1 && index <= 2 {
+			UnmarshalK8SYaml(t, backendConfigStr, &backendConfig)
+			if index == 1 {
+				verifyBackendConfig(t, &backendConfig, "pega-web", 8080)
+			} else {
+				// web and stream health check will happen on 8080 port
+				verifyBackendConfig(t, &backendConfig, "pega-stream", 8080)
+			}
+		}
+	}
+}
 
-	require.Equal(t, "pega-backend-config", backendConfig.Name)
+func verifyBackendConfig(t *testing.T, backendConfig *v1beta1.BackendConfig, name string, port int) {
+	require.Equal(t, name, backendConfig.Name)
 	require.Equal(t, 40, int(*backendConfig.Spec.TimeoutSec))
 	require.Equal(t, 60, int(backendConfig.Spec.ConnectionDraining.DrainingTimeoutSec))
 	require.Equal(t, "GENERATED_COOKIE", backendConfig.Spec.SessionAffinity.AffinityType)
 	require.Equal(t, 3720, int(*backendConfig.Spec.SessionAffinity.AffinityCookieTtlSec))
+
+	require.Equal(t, 5, int(*backendConfig.Spec.HealthCheck.CheckIntervalSec))
+	require.Equal(t, 1, int(*backendConfig.Spec.HealthCheck.HealthyThreshold))
+	require.Equal(t, port, int(*backendConfig.Spec.HealthCheck.Port))
+	require.Equal(t, "/prweb/PRRestService/monitor/pingService/ping", *backendConfig.Spec.HealthCheck.RequestPath)
+	require.Equal(t, 5, int(*backendConfig.Spec.HealthCheck.TimeoutSec))
+	require.Equal(t, "HTTP", *backendConfig.Spec.HealthCheck.Type)
+	require.Equal(t, 2, int(*backendConfig.Spec.HealthCheck.UnhealthyThreshold))
+
 }
