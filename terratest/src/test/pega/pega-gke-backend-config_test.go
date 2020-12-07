9cb3d3f24832299d3a50027e9508b28d259e69e6
Kishor Kumar Vasantala <kishor.vasantal@gmail.com>
2020-12-07 11:32:35 +0530

    ISSUE-135: Using appcontextpath for gke backend config health check & creating tier specific objects (#209)
    * ISSUE-135: Created separate backend config for each ingress resource to use appContextPath & using node name as name of backend config
    
    * ISSUE-135: added tier wise gke back config verification
    
    * ISSUE-135: Fixed tier service test
    
    * ISSUE-135: reverted temp config changes
    
    * ISSUE-135: reset config files
    
    * ISSUE-135: removed debug statements
    
    Co-authored-by: vasak <KishoreKumar.Vasantala@in.pega.com>

--- terratest/src/test/pega/pega-gke-backend-config_test.go
+++ terratest/src/test/pega/pega-gke-backend-config_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"k8s.io/ingress-gce/pkg/apis/backendconfig/v1beta1"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -25,19 +26,38 @@ func TestPegaGKEBackendConfig(t *testing.T) {
 		    }
 
 			yamlContent := RenderTemplate(t, options, helmChartPath, []string{"templates/pega-gke-backend-config.yaml"})
-			verifyBackendConfig(t, yamlContent)			
-
+			verifyBackendConfigs(t, yamlContent)
 		}
-
 }
 
-func verifyBackendConfig(t *testing.T, yamlContent string) {
+func verifyBackendConfigs(t *testing.T, yamlContent string) {
 	var backendConfig v1beta1.BackendConfig
-	UnmarshalK8SYaml(t, yamlContent, &backendConfig)
+	backendConfigSlice := strings.Split(yamlContent, "---")
+	for index, backendConfigStr := range backendConfigSlice {
+		if index >= 1 && index <= 2 {
+			UnmarshalK8SYaml(t, backendConfigStr, &backendConfig)
+			if index == 1 {
+				verifyBackendConfig(t, &backendConfig, "pega-web", 8080)
+			} else {
+				// web and stream health check will happen on 8080 port
+				verifyBackendConfig(t, &backendConfig, "pega-stream", 8080)
+			}
+		}
+	}
+}
 
-	require.Equal(t, "pega-backend-config", backendConfig.Name)
+func verifyBackendConfig(t *testing.T, backendConfig *v1beta1.BackendConfig, name string, port int) {
+	require.Equal(t, name, backendConfig.Name)
 	require.Equal(t, 40, int(*backendConfig.Spec.TimeoutSec))
 	require.Equal(t, 60, int(backendConfig.Spec.ConnectionDraining.DrainingTimeoutSec))
 	require.Equal(t, "GENERATED_COOKIE", backendConfig.Spec.SessionAffinity.AffinityType)
 	require.Equal(t, 3720, int(*backendConfig.Spec.SessionAffinity.AffinityCookieTtlSec))
+
+	require.Equal(t, 5, int(*backendConfig.Spec.HealthCheck.CheckIntervalSec))
+	require.Equal(t, 1, int(*backendConfig.Spec.HealthCheck.HealthyThreshold))
+	require.Equal(t, port, int(*backendConfig.Spec.HealthCheck.Port))
+	require.Equal(t, "/prweb/PRRestService/monitor/pingService/ping", *backendConfig.Spec.HealthCheck.RequestPath)
+	require.Equal(t, 5, int(*backendConfig.Spec.HealthCheck.TimeoutSec))
+	require.Equal(t, "HTTP", *backendConfig.Spec.HealthCheck.Type)
+	require.Equal(t, 2, int(*backendConfig.Spec.HealthCheck.UnhealthyThreshold))
 }
