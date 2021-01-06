ff0b8c111a2dc8982e62eed4d7767d4f1f40baa6
arugm <Madhuri.Arugula@in.pega.com>
2021-01-06 11:25:06 +0530

    Updated README and added assertions to ALB serviceaccount

--- terratest/src/test/addons/alb-ingress_test.go
+++ terratest/src/test/addons/alb-ingress_test.go
@@ -54,24 +54,6 @@ func Test_checkSetAwsRegion(t *testing.T) {
 	require.Contains(t, deployment.Spec.Template.Spec.Containers[0].Args, "--aws-region=YOUR_EKS_CLUSTER_REGION")
 }
 
-func Test_checkAutoDiscoverAwsRegion(t *testing.T) {
-	helmChart := NewHelmConfigParser(
-		NewHelmTest(t, helmChartRelativePath, map[string]string{
-			"aws-load-balancer-controller.enabled":               "true",
-			"aws-load-balancer-controller.autoDiscoverAwsRegion": "true",
-			"aws-load-balancer-controller.region":             "YOUR_EKS_CLUSTER_REGION",
-		}),
-	)
-
-	var deployment *v1.Deployment
-	helmChart.Find(SearchResourceOption{
-		Name: "pega-aws-load-balancer-controller",
-		Kind: "Deployment",
-	}, &deployment)
-
-	require.Contains(t, deployment.Spec.Template.Spec.Containers[0].Args, "--aws-region=YOUR_EKS_CLUSTER_REGION")
-}
-
 func Test_checkSetAwsVpcID(t *testing.T) {
 	helmChart := NewHelmConfigParser(
 		NewHelmTest(t, helmChartRelativePath, map[string]string{
@@ -87,6 +69,21 @@ func Test_checkSetAwsVpcID(t *testing.T) {
 	}, &deployment)
 
 	require.Contains(t, deployment.Spec.Template.Spec.Containers[0].Args, "--aws-vpc-id=YOUR_EKS_CLUSTER_VPC_ID")
+}
+
+func Test_checkSetServiceAnnotation(t *testing.T) {
+	helmChart := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
+			"aws-load-balancer-controller.enabled":              "true",
+		}),
+	)
+	var serviceAccount *corev1.ServiceAccount
+	helmChart.Find(SearchResourceOption{
+		Name: "pega-aws-load-balancer-controller",
+		Kind: "ServiceAccount",
+	}, &serviceAccount)
+
+	require.Contains(t, serviceAccount.ObjectMeta.Annotations["eks.amazonaws.com/role-arn"], "YOUR_IAM_ROLE_ARN")
 }
 
 func Test_checkSetClusterName(t *testing.T) {
