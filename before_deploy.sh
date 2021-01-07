58c6af70e132d4fe58192bc5facf5ba804905629
VinayKumar KsheeraSagar <73693854+pega-kshev@users.noreply.github.com>
2021-01-07 15:37:50 +0530

    US-389994 Initial helm charts SRS backing service (#211)
    * US-389994 Initial helm charts SRS backing service
    
    * US-389994 Template tests added
    
    * US-389994 Readme doc
    
    * US-389994 Internal review changes
    
    * US-389994 yamllint validation fix
    
    * US-389994 Adding tests to travis yaml
    
    * Update CODEOWNERS
    
    * US-389994 Documentation updates for SRS configuration
    
    * US-389994 SRS docker image name change
    
    * US-389994 Doc review changes
    
    * US-389994 addressing review comments
    
    * US-389994 Control network internet egress
    
    * US-389994 yamlint correction for comment space
    
    * US-389994 Code review changes
    
    * US-389994 Internet egress condition check
    
    * US-389994 Using UnmarshalK8SYaml to catch exceptions in go test parser
    
    Co-authored-by: Dave Casavant <dcasavant@gmail.com>

--- before_deploy.sh
+++ before_deploy.sh
@@ -5,8 +5,10 @@
 export CHART_VERSION=$(expr ${TRAVIS_TAG:1})
 export PEGA_FILE_NAME=pega-${CHART_VERSION}.tgz
 export ADDONS_FILE_NAME=addons-${CHART_VERSION}.tgz
-cat descriptor-template.json | jq '.files[0].includePattern=env.PEGA_FILE_NAME' | jq '.files[0].uploadPattern=env.PEGA_FILE_NAME' | jq '.files[1].includePattern=env.ADDONS_FILE_NAME' | jq '.files[1].uploadPattern=env.ADDONS_FILE_NAME' > descriptor.json
+export BACKINGSERVICES_FILE_NAME=backingservices-${CHART_VERSION}.tgz
+cat descriptor-template.json | jq '.files[0].includePattern=env.PEGA_FILE_NAME' | jq '.files[0].uploadPattern=env.PEGA_FILE_NAME' | jq '.files[1].includePattern=env.ADDONS_FILE_NAME' | jq '.files[1].uploadPattern=env.ADDONS_FILE_NAME' | jq '.files[1].includePattern=env.BACKINGSERVICES_FILE_NAME' | jq '.files[1].uploadPattern=env.BACKINGSERVICES_FILE_NAME' > descriptor.json
 curl -o index.yaml https://dl.bintray.com/pegasystems/pega-helm-charts/index.yaml
 helm package --version ${CHART_VERSION} ./charts/pega/
 helm package --version ${CHART_VERSION} ./charts/addons/
+helm package --version ${CHART_VERSION} ./charts/backingservices/
 helm repo index --merge index.yaml --url https://dl.bintray.com/pegasystems/pega-helm-charts/ .
