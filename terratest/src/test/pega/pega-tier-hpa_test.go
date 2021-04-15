9cabb57ba987fc09784d5f70d164510e6d65cfc8
Andrew Roskuski <andrew.roskuski@pega.com>
2021-04-15 11:53:41 -0400

    Adjust scaling (#275)
    * diable memory target by default
    
    * Allow configuring CPU targt as absolute value
    
    * write unit test for autoscaling overrides
    
    * fix lint error
    
    * delete empty value
    
    * remove unused field
    
    * remove unnecessary condition
    
    * Update README based on feedback

--- terratest/src/test/pega/pega-tier-hpa_test.go
+++ terratest/src/test/pega/pega-tier-hpa_test.go
@@ -1,60 +1,28 @@
 package pega
 
 import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/resource"
 	autoscaling "k8s.io/kubernetes/pkg/apis/autoscaling"
 	api "k8s.io/kubernetes/pkg/apis/core"
-	"path/filepath"
-	"testing"
-	"strings"
-	"fmt"
 )
 
-
-
 func TestPegaTierHPA(t *testing.T) {
-	var supportedVendors = []string{"k8s", "openshift", "eks","gke","aks","pks"}
-	var supportedOperations =  []string{"deploy","install-deploy","upgrade-deploy"}
+	var supportedVendors = []string{"k8s", "openshift", "eks", "gke", "aks", "pks"}
+	var supportedOperations = []string{"deploy", "install-deploy", "upgrade-deploy"}
 
 	helmChartPath, err := filepath.Abs(PegaHelmChartPath)
 	require.NoError(t, err)
 
-	for _,vendor := range supportedVendors{
+	for _, vendor := range supportedVendors {
 
-		for _,operation := range supportedOperations{
-
-			fmt.Println(vendor + "-" + operation)
-
-			var options = &helm.Options{			
-				SetValues: map[string]string{
-					"global.provider":        vendor,
-					"global.actions.execute": operation,
-			 	},
-		    }
-
-			yamlContent := RenderTemplate(t, options, helmChartPath, []string{"templates/pega-tier-hpa.yaml"})
-			VerifyPegaHPAs(t,yamlContent, options)
-
-		}
-	}
-
-
-}
-
-func TestPegaTierHPADisableTarget(t *testing.T) {
-	var supportedVendors = []string{"k8s", "openshift", "eks","gke","aks","pks"}
-	var supportedOperations =  []string{"deploy","install-deploy","upgrade-deploy"}
-
-	helmChartPath, err := filepath.Abs(PegaHelmChartPath)
-	require.NoError(t, err)
-
-	testsPath,err := filepath.Abs(PegaHelmChartTestsPath)
-	require.NoError(t, err)
-
-	for _,vendor := range supportedVendors{
-
-		for _,operation := range supportedOperations{
+		for _, operation := range supportedOperations {
 
 			fmt.Println(vendor + "-" + operation)
 
@@ -65,66 +33,168 @@ func TestPegaTierHPADisableTarget(t *testing.T) {
 				},
 			}
 
-			yamlContent := RenderTemplate(t, options, helmChartPath, []string{"templates/pega-tier-hpa.yaml"}, "--values" , testsPath + "/data/values_hpa_disabletarget.yaml")
-			VerifyTargets(t, yamlContent)
+			yamlContent := RenderTemplate(t, options, helmChartPath, []string{"templates/pega-tier-hpa.yaml"})
+			verifyPegaHPAs(t, yamlContent, options, []hpa{
+				{
+					name:          "pega-web-hpa",
+					targetRefName: "pega-web",
+					kind:          "Deployment",
+					apiversion:    "apps/v1",
+					cpu:           true,
+					cpuValue:      parseResourceValue(t, "2.8"),
+				},
+				{
+					name:          "pega-batch-hpa",
+					targetRefName: "pega-batch",
+					kind:          "Deployment",
+					apiversion:    "apps/v1",
+					cpu:           true,
+					cpuValue:      parseResourceValue(t, "2.8"),
+				},
+			})
+
 		}
 	}
+
 }
 
-// VerifyPegaHPAs - Splits the HPA object from the rendered template and asserts each HPA object
-func VerifyPegaHPAs(t *testing.T, yamlContent string, options *helm.Options) {
-	var pegaHpaObj autoscaling.HorizontalPodAutoscaler
-	hpaSlice := strings.SplitAfter(yamlContent, "85")
-	for index, hpaInfo := range hpaSlice {
-		if index >= 0 && index <= 1 {
-			UnmarshalK8SYaml(t, hpaInfo, &pegaHpaObj)
-			if index == 0 {
-				VerifyPegaHpa(t, &pegaHpaObj, hpa{"pega-web-hpa", "pega-web", "Deployment", "apps/v1"}, true, true)
-			} else {
-				VerifyPegaHpa(t, &pegaHpaObj, hpa{"pega-batch-hpa", "pega-batch", "Deployment", "apps/v1"}, true, false)
+func TestPegaTierHPADisableTarget(t *testing.T) {
+	var supportedVendors = []string{"k8s", "openshift", "eks", "gke", "aks", "pks"}
+	var supportedOperations = []string{"deploy", "install-deploy", "upgrade-deploy"}
+
+	helmChartPath, err := filepath.Abs(PegaHelmChartPath)
+	require.NoError(t, err)
+
+	testsPath, err := filepath.Abs(PegaHelmChartTestsPath)
+	require.NoError(t, err)
+
+	for _, vendor := range supportedVendors {
+
+		for _, operation := range supportedOperations {
+
+			fmt.Println(vendor + "-" + operation)
+
+			var options = &helm.Options{
+				SetValues: map[string]string{
+					"global.provider":        vendor,
+					"global.actions.execute": operation,
+				},
 			}
+
+			yamlContent := RenderTemplate(t, options, helmChartPath, []string{"templates/pega-tier-hpa.yaml"}, "--values", testsPath+"/data/values_hpa_disabletarget.yaml")
+			verifyPegaHPAs(t, yamlContent, options, []hpa{
+				{
+					name:          "pega-web-hpa",
+					targetRefName: "pega-web",
+					kind:          "Deployment",
+					apiversion:    "apps/v1",
+					mem:           true,
+					memPercent:    85,
+				},
+				{
+					name:          "pega-batch-hpa",
+					targetRefName: "pega-batch",
+					kind:          "Deployment",
+					apiversion:    "apps/v1",
+					cpu:           true,
+					cpuValue:      parseResourceValue(t, "2.8"),
+				},
+			})
 		}
 	}
 }
 
-// VerifyPegaHpa - Performs Pega HPA assertions with the values as provided in default values.yaml
-func VerifyPegaHpa(t *testing.T, hpaObj *autoscaling.HorizontalPodAutoscaler, expectedHpa hpa, shouldHaveCpuTarget bool, shouldHaveMemoryTarget bool) {
+func TestPegaTierOverrideValues(t *testing.T) {
+	var supportedVendors = []string{"k8s", "openshift", "eks", "gke", "aks", "pks"}
+	var supportedOperations = []string{"deploy", "install-deploy", "upgrade-deploy"}
+
+	helmChartPath, err := filepath.Abs(PegaHelmChartPath)
+	require.NoError(t, err)
+
+	testsPath, err := filepath.Abs(PegaHelmChartTestsPath)
+	require.NoError(t, err)
+
+	for _, vendor := range supportedVendors {
+
+		for _, operation := range supportedOperations {
+
+			fmt.Println(vendor + "-" + operation)
+
+			var options = &helm.Options{
+				SetValues: map[string]string{
+					"global.provider":        vendor,
+					"global.actions.execute": operation,
+				},
+			}
+
+			yamlContent := RenderTemplate(t, options, helmChartPath, []string{"templates/pega-tier-hpa.yaml"}, "--values", testsPath+"/data/values_hpa_overridevalues.yaml")
+			verifyPegaHPAs(t, yamlContent, options, []hpa{
+				{
+					name:          "pega-web-hpa",
+					targetRefName: "pega-web",
+					kind:          "Deployment",
+					apiversion:    "apps/v1",
+					cpu:           true,
+					cpuValue:      parseResourceValue(t, "4.13"),
+					mem:           true,
+					memPercent:    42,
+				},
+				{
+					name:          "pega-batch-hpa",
+					targetRefName: "pega-batch",
+					kind:          "Deployment",
+					apiversion:    "apps/v1",
+					cpu:           true,
+					cpuPercent:    24,
+				},
+			})
+		}
+	}
+}
+
+// verifyPegaHPAs - Splits the HPA object from the rendered template and asserts each HPA object
+func verifyPegaHPAs(t *testing.T, yamlContent string, options *helm.Options, expectedHpas []hpa) {
+	var pegaHpaObj autoscaling.HorizontalPodAutoscaler
+	hpaSlice := strings.SplitAfter(yamlContent, "---")
+	hpaSlice = hpaSlice[1:]
+
+	require.Equal(t, len(expectedHpas), len(hpaSlice))
+
+	for i, hpa := range hpaSlice {
+		UnmarshalK8SYaml(t, hpa, &pegaHpaObj)
+		verifyPegaHpa(t, &pegaHpaObj, expectedHpas[i])
+	}
+}
+
+// verifyPegaHpa - Performs Pega HPA assertions with the values as provided in default values.yaml
+func verifyPegaHpa(t *testing.T, hpaObj *autoscaling.HorizontalPodAutoscaler, expectedHpa hpa) {
 	require.Equal(t, hpaObj.Spec.ScaleTargetRef.Name, expectedHpa.targetRefName)
 	require.Equal(t, hpaObj.Spec.ScaleTargetRef.Kind, expectedHpa.kind)
 	require.Equal(t, hpaObj.Spec.ScaleTargetRef.APIVersion, expectedHpa.apiversion)
-	if shouldHaveCpuTarget {
-		require.Equal(t, hpaObj.Spec.Metrics[0].Resource.Name, api.ResourceName("cpu"))
-	}
-	if shouldHaveMemoryTarget {
-		require.Equal(t, hpaObj.Spec.Metrics[1].Resource.Name, api.ResourceName("memory"))
-	}
-	require.Equal(t, hpaObj.Spec.MaxReplicas, int32(5))
-}
+	currentMetricIndex := 0
 
+	require.Equal(t, expectedHpa.expectedMetricCount(), len(hpaObj.Spec.Metrics))
 
-func VerifyTargets(t *testing.T, yamlContent string) {
-	var pegaHpaObj autoscaling.HorizontalPodAutoscaler
-	hpaSlice := strings.SplitAfter(yamlContent, "85")
-	for index, hpaInfo := range hpaSlice {
-		if index >= 0 && index <= 1 {
-			UnmarshalK8SYaml(t, hpaInfo, &pegaHpaObj)
-			if index == 0 {
-				VerifyWebNoCpuTarget(t, &pegaHpaObj)
-			} else {
-				VerifyBatchNoMemoryTarget(t, &pegaHpaObj)
-			}
+	if expectedHpa.cpu {
+		require.Equal(t, api.ResourceName("cpu"), hpaObj.Spec.Metrics[currentMetricIndex].Resource.Name)
+		if expectedHpa.cpuValue != (resource.Quantity{}) {
+			require.Equal(t, autoscaling.MetricTargetType("Value"), hpaObj.Spec.Metrics[currentMetricIndex].Resource.Target.Type)
+			require.Equal(t, expectedHpa.cpuValue, *hpaObj.Spec.Metrics[currentMetricIndex].Resource.Target.AverageValue)
 		}
+		if expectedHpa.cpuPercent != 0 {
+			require.Equal(t, autoscaling.MetricTargetType("Utilization"), hpaObj.Spec.Metrics[currentMetricIndex].Resource.Target.Type)
+			require.Equal(t, expectedHpa.cpuPercent, *hpaObj.Spec.Metrics[currentMetricIndex].Resource.Target.AverageUtilization)
+		}
+		currentMetricIndex++
 	}
-}
+	if expectedHpa.mem {
+		require.Equal(t, api.ResourceName("memory"), hpaObj.Spec.Metrics[currentMetricIndex].Resource.Name)
+		require.Equal(t, autoscaling.MetricTargetType("Utilization"), hpaObj.Spec.Metrics[currentMetricIndex].Resource.Target.Type)
+		require.Equal(t, expectedHpa.memPercent, *hpaObj.Spec.Metrics[currentMetricIndex].Resource.Target.AverageUtilization)
+		currentMetricIndex++
+	}
 
-func VerifyWebNoCpuTarget(t *testing.T, pegaHpaObj *autoscaling.HorizontalPodAutoscaler) {
-	require.Equal(t, 1, len(pegaHpaObj.Spec.Metrics))
-	require.Equal(t, pegaHpaObj.Spec.Metrics[0].Resource.Name, api.ResourceName("memory"))
-}
-
-func VerifyBatchNoMemoryTarget(t *testing.T, pegaHpaObj *autoscaling.HorizontalPodAutoscaler) {
-	require.Equal(t,1, len(pegaHpaObj.Spec.Metrics))
-	require.Equal(t, pegaHpaObj.Spec.Metrics[0].Resource.Name, api.ResourceName("cpu"))
+	require.Equal(t, int32(5), hpaObj.Spec.MaxReplicas)
 }
 
 type hpa struct {
@@ -132,4 +202,32 @@ type hpa struct {
 	targetRefName string
 	kind          string
 	apiversion    string
+	cpu           bool
+	cpuValue      resource.Quantity
+	cpuPercent    int32
+	mem           bool
+	memPercent    int32
+}
+
+func (h hpa) expectedMetricCount() int {
+	result := 0
+	if h.cpu {
+		result++
+	}
+
+	if h.mem {
+		result++
+	}
+
+	return result
+}
+
+func parseResourceValue(t *testing.T, resourceString string) resource.Quantity {
+	value, err := resource.ParseQuantity(resourceString)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	return value
 }
