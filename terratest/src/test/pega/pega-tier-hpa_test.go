594b886dcaf81b72a9e8653d960f802cf22d24be
kants3 <shashi.kant@in.pega.com>
2021-04-28 18:31:32 +0530

    corrected test case

--- terratest/src/test/pega/pega-tier-hpa_test.go
+++ terratest/src/test/pega/pega-tier-hpa_test.go
@@ -138,6 +138,7 @@ func TestPegaTierOverrideValues(t *testing.T) {
                     SetValues: map[string]string{
                         "global.provider":        vendor,
                         "global.actions.execute": operation,
+						"installer.upgrade.upgradeType": "zero-downtime",
                     },
                 }
 
