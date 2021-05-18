c466bd4924d00febf80f289a612f2090dd282824
Anand <Anandrao.kotamsetti@in.pega.com>
2021-05-18 19:13:29 +0530

    updated srs-deployment_test.go with newly added params for SRS

--- terratest/src/test/backingservices/srs-deployment_test.go
+++ terratest/src/test/backingservices/srs-deployment_test.go
@@ -166,8 +166,17 @@ func VerifyDeployment(t *testing.T, pod *k8score.PodSpec, expectedSpec srsDeploy
 	require.Equal(t, "ELASTICSEARCH_PROTO", pod.Containers[0].Env[envIndex].Name)
 	require.Equal(t, expectedSpec.elasticsearchEndPoint.protocol, pod.Containers[0].Env[envIndex].Value)
 	envIndex++
-	require.Equal(t, "APPLICATION_HOST", pod.Containers[0].Env[envIndex].Name)
-	require.Equal(t, "0.0.0.0", pod.Containers[0].Env[envIndex].Value)
+	require.Equal(t, "ELASTICSEARCH_AUTH_PROVIDER", pod.Containers[0].Env[envIndex].Name)
+	require.Equal(t, "none", pod.Containers[0].Env[envIndex].Value)
+	envIndex++
+	require.Equal(t, "ELASTICSEARCH_REGION", pod.Containers[0].Env[envIndex].Name)
+	require.Equal(t, "NONE", pod.Containers[0].Env[envIndex].Value)
+	envIndex++
+	require.Equal(t, "ELASTICSEARCH_USERNAME", pod.Containers[0].Env[envIndex].Name)
+	require.Equal(t, "NONE", pod.Containers[0].Env[envIndex].Value)
+	envIndex++
+	require.Equal(t, "ELASTICSEARCH_PASSWORD", pod.Containers[0].Env[envIndex].Name)
+	require.Equal(t, "NONE", pod.Containers[0].Env[envIndex].Value)
 	envIndex++
 	require.Equal(t, "APPLICATION_PORT", pod.Containers[0].Env[envIndex].Name)
 	require.Equal(t, "8080", pod.Containers[0].Env[envIndex].Value)
