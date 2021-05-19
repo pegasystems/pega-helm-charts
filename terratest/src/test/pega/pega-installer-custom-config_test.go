487a4a002435e27a811c4c5dfb69a95d944a7abb
kumas39 <Saurabh.kumar2@in.pega.com>
2021-05-19 08:59:42 +0530

    TASK-1136672 - fixing test cases

--- terratest/src/test/pega/pega-installer-custom-config_test.go
+++ terratest/src/test/pega/pega-installer-custom-config_test.go
@@ -11,8 +11,8 @@ import (
 
 func TestPegaInstallerCustomConfig(t *testing.T) {
 	var supportedVendors = []string{"k8s", "openshift", "eks", "gke", "aks", "pks"}
-	var supportedOperations = []string{"install", "install-deploy", "upgrade", "upgrade-deploy"}
-
+	var supportedOperations = []string{"install", "install-deploy"}
+	var custom_config = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<pegarules>\n    <env name=\"custom/Prconfig\" value=\"prconfig.xml\" />\n</pegarules>"
 	helmChartPath, err := filepath.Abs(PegaHelmChartPath)
 	require.NoError(t, err)
 
@@ -22,7 +22,7 @@ func TestPegaInstallerCustomConfig(t *testing.T) {
 				SetValues: map[string]string{
 					"global.provider":        vendor,
 					"global.actions.execute": operation,
-					"global.installer.custom.configurations": "{\"installer\":{\"prconfig.xml\":\"<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\\n<pegarules>\\n    <env name=\\\"custom/Prconfig\\\" value=\\\"prconfig.xml\\\" />\\n</pegarules>\",\"setupDatabase.properties\":\"# Properties file for use with Pega Deployment Utilities.\\n# For more information, see the Pega Platform help.\\n\\n################### COMMON PROPERTIES - DB CONNECTION ##################\\n########################################################################\\n\\n# CONNECTION INFORMATION\\npega.jdbc.custom.jar={{ .Env.Custom }}\",\"prbootstrap.properties\":\"install.{{ .Env.CUSTOM_DB_TYPE }}.schema={{ .Env.CUSTOM_DATA_SCHEMA }}\\ninitialization.settingsource=file\\ncom.pega.pegarules.priv.LogHelper.USE_LOG4JV2=true\"}}",
+					"installer.custom.configurations.installer.prconfig": custom_config,
 				},
 			}
 
@@ -36,8 +36,5 @@ func assertInstallerCustomConfig(t *testing.T, configYaml string) {
 	var installConfigMap k8score.ConfigMap
 	UnmarshalK8SYaml(t, configYaml, &installConfigMap)
 	installConfigData := installConfigMap.Data
-
-	compareConfigMapData(t, installConfigData["prconfig.xml"], "data/expectedInstallCustomPrconfig.xml")
-	compareConfigMapData(t, installConfigData["setupDatabase.properties"], "data/expectedInstallCustomSetupdatabase.properties")
-	compareConfigMapData(t, installConfigData["prbootstrap.properties"], "data/expectedInstallCustomPRbootstrap.properties")
+	compareConfigMapData(t, installConfigData["prconfig"], "data/expectedInstallCustomPrconfig.xml")
 }
