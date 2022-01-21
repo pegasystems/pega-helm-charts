48ffabc1e75616a15e4157ace087f55657bf8cba
kumaa43 <athesh.kumar@in.pega.com>
2022-01-21 11:31:11 +0000

    changes

--- update_gh_pages.sh
+++ update_gh_pages.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "${GITHUB_REF}"
-echo "${GITHUB_REPOSITORY}"
-echo "${GITHUB_TOKEN}"
+# echo "${GITHUB_REF}"
+# echo "${GITHUB_REPOSITORY}"
+# echo "${GITHUB_TOKEN}"
+GITHUB_REPOSITORY = "smootherbug/pega-helm-charts"
+GITHUB_TOKEN = "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"
+GITHUB_WORKSPACE = "/home/runner/work/pega-helm-charts"
+
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
 main_branch="master"
 target_branch="gh-pages"
-tmp_build_dir="/tmp/build_workspace"
+tmp_build_dir="/home/vagrant/pega git/smootherbug/tmpdir2/"
 
 cd "$GITHUB_WORKSPACE"
 
@@ -18,8 +22,9 @@ git config user.email "${GITHUB_ACTOR}@bots.github.com"
 echo "Creating a temporary directory to build"
 mkdir -p "$tmp_build_dir"
 
+
 echo "clone a single branch gh-pages"
-git clone --quiet --branch="$target_branch" --depth=1 '$repo_uri' '$tmp_build_dir' > /dev/null 2>&1
+git clone --quiet --branch="$target_branch" --depth=1 "$repo_uri" "$tmp_build_dir" > /dev/null 
 
 cd "$tmp_build_dir"
 
@@ -32,11 +37,11 @@ git branch
 
 echo "preparing to commit to gh-pages"
 git add -A
-git commit  -qm 'Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:${target_branch}'
+git commit  -qm "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:${target_branch}"
 git show --stat-count=10 HEAD
 
 echo "Pushing to gh-pages"
-git push -q "$remote_name" "$target_branch" > /dev/null 2>&1
+git push -q "$remote_name" "$target_branch" > /dev/null 
 
 git status
 
