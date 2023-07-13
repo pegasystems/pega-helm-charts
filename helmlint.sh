9ad305d302609a9ddee33441039d2d05a2664549
yeuna2 <alexander.yeung@pega.com>
2023-07-13 16:03:02 -0400

    Adjusted syntax pt.2

--- helmlint.sh
+++ helmlint.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-chart_names="addons" "backingservices" "pega"
-values_files="test_tls.yml" "values.yml"
-providers="k8s" "openshift" "eks" "gke" "pks" "aks"
-
 # Nested for loops to iterate over the chart names, values files, and providers
-for chart_name in "${chart_names[@]}"
+for chart_name in "addons" "backingservices" "pega"
 do
-    for values_file in "${values_files[@]}"
+    for values_file in "test_tls.yml" "values.yml"
     do
-        for provider in "${providers[@]}"
+        for provider in "k8s" "openshift" "eks" "gke" "pks" "aks"
         do
             helm lint --with-subcharts --values "lint/$values_file" --set-string "global.provider=$provider" --strict "charts/$chart_name"
         done
