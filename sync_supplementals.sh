9315365ddab35970535d7b04a1b550df18147a85
yeuna2 <alexander.yeung@pega.com>
2023-07-18 16:30:34 -0400

    Adjusted bash script syntax pt.2

--- sync_supplementals.sh
+++ sync_supplementals.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 master_file="charts/pega/templates/_supplemental.tpl"
-tpl_files="charts/pega/charts/installer/templates/_supplemental.tpl"
-"charts/pega/charts/hazelcast/templates/_supplemental.tpl"
-"charts/pega/charts/pegasearch/templates/_supplemental.tpl"
-
-IFS='
-'
-
 
 # Loop through each TPL file
-for tpl_file in "${tpl_files[@]}"; do
+for tpl_file in "charts/pega/charts/installer/templates/_supplemental.tpl" "charts/pega/charts/hazelcast/templates/_supplemental.tpl" "charts/pega/charts/pegasearch/templates/_supplemental.tpl"; do
   # Compare the TPL file with the master file
   if ! cmp -s "$master_file" "$tpl_file"; then
     # Sync the master file to the TPL copy
