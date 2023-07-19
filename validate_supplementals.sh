992c71388cfecf75d8cfebd1b39f24940e1943b2
yeuna2 <alexander.yeung@pega.com>
2023-07-19 14:45:23 -0400

    Restructured github-actions-build. Added subchart template validation checks.

--- validate_supplementals.sh
+++ validate_supplementals.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 master_file="charts/pega/templates/_supplemental.tpl"
+validation_failed=0
+failed_files=""
 
 # Loop through each TPL file
 for tpl_file in "charts/pega/charts/installer/templates/_supplemental.tpl" "charts/pega/charts/hazelcast/templates/_supplemental.tpl" "charts/pega/charts/pegasearch/templates/_supplemental.tpl"; do
   # Compare the TPL file with the master file
   if ! cmp -s "$master_file" "$tpl_file"; then
-    # Sync the master file to the TPL copy
-    cp "$master_file" "$tpl_file"
-    echo "Synced $tpl_file with master file."
+    validation_failed=1
+    failed_files="$failed_files\n$tpl_file"
   fi
 done
+
+#Check validation status
+if [ $validation_failed -eq 1 ]; then
+  echo -e "Validatoin failed for the following files:"
+  echo -e "$failed_files"
+  exit 1
+else
+  echo "Validation passed: All files match the master file."
+  exit 0
+fi
+
