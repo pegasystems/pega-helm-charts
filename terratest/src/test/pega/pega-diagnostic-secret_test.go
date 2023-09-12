b553e4db08087197dfeaec6a2cb8319a91f3d1a2
rajuu <UmaveeraBasaveswararao.Rajulapati@pega.com>
2023-09-12 19:14:11 +0530

    Revert "GitGuardian issue fix"
    This reverts commit 7fd2baf4ce390f857a0e31949eebff4c15a41b57.

--- terratest/src/test/pega/pega-diagnostic-secret_test.go
+++ terratest/src/test/pega/pega-diagnostic-secret_test.go
@@ -10,8 +10,8 @@ import (
 	k8score "k8s.io/api/core/v1"
 )
 
-const diagnosticWebUser = "ZGlhZ25vc3RpY1dlYlVzZXI="
-const diagnosticWebPassword = "ZGlhZ25vc3RpY1dlYlVzZXI="
+const diagnosticWebUser = "diagnosticWebUser"
+const diagnosticWebPassword = "diagnosticWebPassword"
 
 func TestPegaDiagnosticSecret(t *testing.T) {
 	var supportedVendors = []string{"k8s", "openshift", "eks", "gke", "aks", "pks"}
