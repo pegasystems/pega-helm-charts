e63e3b4a3e74ba20e054a3f56bcdc8050012e5be
maracle6 <45719028+maracle6@users.noreply.github.com>
2023-10-18 12:29:17 -0700

    ISSUE-651 add basic unit tests

--- terratest/src/test/backingservices/constellation_test.go
+++ terratest/src/test/backingservices/constellation_test.go
@@ -35,6 +35,60 @@ func Test_shouldContainConstellationResourcesWhenEnabled(t *testing.T) {
 	}
 }
 
+func Test_shouldContainConstellationResourcesWithOptionalValues(t *testing.T) {
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
+			"constellation.enabled": "true",
+			"constellation.customerAssetVolumeClaimName": "claim-name",
+			"constellation.ingressAnnotations": "annotation",
+			"constellation.imagePullSecretNames": "{secret1, secret2}",
+		}),
+	)
+
+	for _, i := range constellationResources {
+		require.True(t, helmChartParser.Contains(SearchResourceOption{
+			Name: i.Name,
+			Kind: i.Kind,
+		}))
+	}
+}
+
+func Test_shouldContainConstellationCdnWhenEnabled(t *testing.T) {
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
+			"constellation.constellation-cdn.enabled": "true",
+			"constellation.constellation-cdn.name": "constellation-cdn",
+			"constellation.constellation-cdn.imagePullSecretNames": "{secret1, secret2}",
+			"constellation.constellation-cdn.ingress.domain": "test.com",
+		}),
+	)
+
+	for _, i := range constellationCdnResources {
+		require.True(t, helmChartParser.Contains(SearchResourceOption{
+			Name: i.Name,
+			Kind: i.Kind,
+		}))
+	}
+}
+
+func Test_shouldNotContainConstellationCdnWhenDisabled(t *testing.T) {
+	helmChartParser := NewHelmConfigParser(
+		NewHelmTest(t, helmChartRelativePath, map[string]string{
+			"constellation.constellation-cdn.enabled": "false",
+			"constellation.constellation-cdn.name": "constellation-cdn",
+			"constellation.constellation-cdn.imagePullSecretNames": "{secret1, secret2}",
+			"constellation.constellation-cdn.ingress.domain": "test.com",
+		}),
+	)
+
+	for _, i := range constellationCdnResources {
+		require.False(t, helmChartParser.Contains(SearchResourceOption{
+			Name: i.Name,
+			Kind: i.Kind,
+		}))
+	}
+}
+
 var constellationResources = []SearchResourceOption{
 	{
 		Name: "constellation",
@@ -51,5 +105,20 @@ var constellationResources = []SearchResourceOption{
 	{
 		Name: "constellation-registry-secret",
 		Kind: "Secret",
+	},
+}
+
+var constellationCdnResources = []SearchResourceOption{
+	{
+		Name: "constellation-cdn-881",
+		Kind: "Deployment",
+	},
+	{
+		Name: "constellation-cdn-881",
+		Kind: "Service",
+	},
+	{
+		Name: "constellation-cdn",
+		Kind: "Ingress",
 	},
 }
