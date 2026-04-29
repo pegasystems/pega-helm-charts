53c118f650f12342583aee5f5e95eb72e1e142f0
misterdorito <mrdorito@gmail.com>
2026-04-29 16:25:18 -0400

    Conditionalize downloader script config map to only be rendered for the appropriate global.actions.execute values

--- terratest/src/test/pega/pega-installer-lib-download-script-config_test.go
+++ terratest/src/test/pega/pega-installer-lib-download-script-config_test.go
@@ -16,7 +16,7 @@ func TestPegaInstallerLibDownloadScriptConfig(t *testing.T) {
         SetValues: map[string]string{
             "global.deployment.name":        "pega",
             "global.provider":               "k8s",
-            "global.actions.execute":        "deploy",
+            "global.actions.execute":        "install",
             "global.downloadContainer.image": "ICDOWNLOAD_IMAGE:1.0",
         },
     }
@@ -27,4 +27,32 @@ func TestPegaInstallerLibDownloadScriptConfig(t *testing.T) {
 
     require.Equal(t, "pega-installer-lib-download-script-config", envConfigMap.ObjectMeta.Name)
     VerifyEnvPresent(t, &envConfigMap, "download-jdbc-lib.sh")
+}
+
+
+func TestPegaInstallerLibDownloadScriptConfigRenderedForActions(t *testing.T) {
+    var supportedOperations = []string{"install", "upgrade", "install-deploy", "upgrade-deploy"}
+
+    helmChartPath, err := filepath.Abs(PegaHelmChartPath)
+	require.NoError(t, err)
+
+    for _, operation := range supportedOperations {
+
+        var options = &helm.Options{
+            SetValues: map[string]string{
+                "global.deployment.name":        "pega",
+                "global.provider":               "k8s",
+                "global.actions.execute":        operation,
+                "installer.upgrade.upgradeType": "out-of-place",
+                "global.downloadContainer.image": "ICDOWNLOAD_IMAGE:1.0",
+            },
+        }
+        yamlContent := RenderTemplate(t, options, helmChartPath, []string{"charts/installer/templates/pega-installer-lib-download-script-config.yaml"})
+
+        var envConfigMap k8score.ConfigMap
+        UnmarshalK8SYaml(t, yamlContent, &envConfigMap)
+
+        require.Equal(t, "pega-installer-lib-download-script-config", envConfigMap.ObjectMeta.Name)
+        VerifyEnvPresent(t, &envConfigMap, "download-jdbc-lib.sh")
+    }
 }
